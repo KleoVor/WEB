36691923915f5e9abac8b7436eb151b223d7c3c8
KleoVor <t9671253993@gmail.com>
2023-08-14 18:01:53 +0300

    верный jar и написаны тесты для Selenide

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,28 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CallbackTest {
+    @Test
+    void shouldTest() throws InterruptedException {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        //     form.$("[data-test-id=\"name\"] input").setValue("Василий");
+
+
+        form.$("[data-test-id=name] input").setValue("Наталья");
+        form.$("[data-test-id=phone] input").setValue("+79670000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button_theme_alfa-on-white").click();
+        // $("[data-test-id="order-success"].shouldHave("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.")");
+        $(".paragraph_theme_alfa-on-white").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        Thread.sleep(5000);
+
+    }
+}
